7e98ed1f8f474424572e580c8b61fabf4e7bc0ac
puneetbhatia77 <puneetbhatia77@gmail.com>
2021-06-30 19:49:56 +0530

    Update ErrorViewModel.cs

--- Models/ErrorViewModel.cs
+++ Models/ErrorViewModel.cs
@@ -1,7 +1,7 @@
 using System;
 
 namespace pipelines_dotnet_core.Models
-{   // test trigger
+{   // test trigger 1
     public class ErrorViewModel
     {
         public string RequestId { get; set; }
